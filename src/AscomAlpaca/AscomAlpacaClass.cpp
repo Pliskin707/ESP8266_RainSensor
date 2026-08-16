a27fbd137d2ae2186598ab24eb563e1c33b4ff69
Pliskin707 <pliskin@x-techs.de>
2026-08-16 20:52:03 +0200

    Transaction IDs added; Connection is now possible (no valid values yet)

--- src/AscomAlpaca/AscomAlpacaClass.cpp
+++ src/AscomAlpaca/AscomAlpacaClass.cpp
@@ -4,6 +4,27 @@
 static JsonDocument _alpaca_doc;
 static ESP8266WebServer _server;
 
+static uint32_t _get_server_transaction_id (void)
+{
+    static uint32_t server_transaction_id = 0;
+
+    // valid values are [1..UINT32_MAX]
+    if(!++server_transaction_id)
+        ++server_transaction_id;
+
+    return server_transaction_id;
+}
+
+static uint32_t _get_client_transaction_id (void)
+{
+    int client_transaction_id = 0;
+
+    if (_server.hasArg("ClientTransactionID"))
+        client_transaction_id = _server.arg("ClientTransactionID").toInt();
+
+    return client_transaction_id;
+}
+
 void ascom_alpaca::_handle_discovery(void)
 {
     int packetSize = _udp.parsePacket();
@@ -53,19 +74,15 @@ const char *ascom_alpaca::get_uid(void)
   return uid;
 }
 
-static void dummy()
-{
-    dprintf("dummy called");
-    _server.send(500, "text/plain", "not implemented");
-}
-
 static void send_api_versions()
 {
-    dprintf("\napiversions called");
-
     _alpaca_doc.clear();
+
     auto versions = _alpaca_doc["Value"].to<JsonArray>();
     versions.add(1);
+
+    _alpaca_doc["ClientTransactionID"] = _get_client_transaction_id();
+    _alpaca_doc["ServerTransactionID"] = _get_server_transaction_id();
 
     _server.sendHeader("Content-Type", "application/json");
     String serialized;
@@ -75,14 +92,16 @@ static void send_api_versions()
 
 static void send_api_description()
 {
-    dprintf("\napi description called");
-
     _alpaca_doc.clear();
+
     auto value = _alpaca_doc["Value"].to<JsonObject>();
     value["ServerName"] = ascom_alpaca::get_uid();
     value["Manufacturer"] = "Pliskin707";
     value["ManufacturerVersion"] = "1.0.0.0";
     value["Location"] = "nearby ;)";
+
+    _alpaca_doc["ClientTransactionID"] = _get_client_transaction_id();
+    _alpaca_doc["ServerTransactionID"] = _get_server_transaction_id();
 
     _server.sendHeader("Content-Type", "application/json");
     String serialized;
@@ -92,9 +111,8 @@ static void send_api_description()
 
 static void send_configured_devices()
 {
-    dprintf("\ncfg devices called");
-
     _alpaca_doc.clear();
+
     auto value = _alpaca_doc["Value"].to<JsonArray>();
     auto item = value.add<JsonObject>();
     item["DeviceName"] = "RainSensor";
@@ -103,6 +121,46 @@ static void send_configured_devices()
     auto unique_id = String(ascom_alpaca::get_uid());
     unique_id += "_0";
     item["UniqueID"] = unique_id;
+
+    _alpaca_doc["ClientTransactionID"] = _get_client_transaction_id();
+    _alpaca_doc["ServerTransactionID"] = _get_server_transaction_id();
+
+    _server.sendHeader("Content-Type", "application/json");
+    String serialized;
+    serializeJson(_alpaca_doc, serialized);
+    _server.send(200, "application/json", serialized);
+}
+
+static void send_connected_state()
+{
+    _alpaca_doc.clear();
+
+    // TODO this has parameters; parse and use them!
+    // TODO this can be PUT or GET; handle them
+    _alpaca_doc["ClientTransactionID"] = _get_client_transaction_id();
+    _alpaca_doc["ServerTransactionID"] = _get_server_transaction_id();
+    _alpaca_doc["ErrorNumber"] = 0;
+    _alpaca_doc["ErrorMessage"] = "";
+    _alpaca_doc["Value"] = true;
+
+    _server.sendHeader("Content-Type", "application/json");
+    String serialized;
+    serializeJson(_alpaca_doc, serialized);
+    _server.send(200, "application/json", serialized);
+}
+
+static void send_rain_rate()
+{
+    dprintf("\n rain rate called");
+
+    // TODO this has parameters; parse and use them!
+    _alpaca_doc.clear();
+
+    _alpaca_doc["ClientTransactionID"] = _get_client_transaction_id();
+    _alpaca_doc["ServerTransactionID"] = _get_server_transaction_id();
+    _alpaca_doc["ErrorNumber"] = 0;
+    _alpaca_doc["ErrorMessage"] = "";
+    _alpaca_doc["Value"] = 0.1;
 
     _server.sendHeader("Content-Type", "application/json");
     String serialized;
@@ -118,7 +176,9 @@ void ascom_alpaca::begin(const uint16_t port_discovery, const uint16_t port_devi
     _server.on("/management/apiversions", send_api_versions);
     _server.on("/management/v1/description", send_api_description);
     _server.on("/management/v1/configureddevices", send_configured_devices);
-    _server.on("/api/v1", dummy);
+    // _server.on("/setup/v1/observingconditions/0/setup", nullptr);    // <- this is called if you press the gears symbol in N.I.N.A.
+    _server.on("/api/v1/observingconditions/0/connected", send_connected_state);
+    _server.on("/api/v1/observingconditions/0/rainrate", send_rain_rate);
 }
 
 void ascom_alpaca::loop(const bool connected)
