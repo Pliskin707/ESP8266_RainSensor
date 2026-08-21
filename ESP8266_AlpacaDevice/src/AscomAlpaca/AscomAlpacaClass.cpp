6ae8b415c93d47c850f74494df950173d6bbf852
Pliskin707 <pliskin@x-techs.de>
2026-08-21 08:23:16 +0200

    added copy for second device (mains powered alpaca device)

---
+++ ESP8266_AlpacaDevice/src/AscomAlpaca/AscomAlpacaClass.cpp
@@ -0,0 +1,425 @@
+#include "AscomAlpacaClass.hpp"
+#include "../projutils/projutils.hpp"
+
+// #define DEBUG_CALL_CYCLE
+#ifndef DEBUG_CALL_CYCLE
+#define DEBUG_CALL_CYCLE \
+static uint32_t last_call = 0;\
+const uint32_t sys_time = millis(), ms_since_last_call = sys_time - (last_call ?: sys_time);\
+last_call = sys_time;\
+dprintf("\n %s state called after %lu ms", __func__, ms_since_last_call);
+#endif
+
+ascom_alpaca alpaca;
+static ESP8266WebServer _server;
+static volatile uint32_t _last_http_api_call = 0uL;
+static float _average_period_hours = 0.25;   // `0.0` must always be accepted (instantaneous value)
+static float _rain_rate = 0.1;
+static bool _is_safe = true;
+
+static uint32_t _get_server_transaction_id (void)
+{
+    static uint32_t server_transaction_id = 0;
+
+    // valid values are [1..UINT32_MAX]
+    if(!++server_transaction_id)
+        ++server_transaction_id;
+
+    return server_transaction_id;
+}
+
+static uint32_t _get_client_transaction_id (void)
+{
+    int client_transaction_id = 0;
+
+    if (_server.hasArg("ClientTransactionID"))
+        client_transaction_id = _server.arg("ClientTransactionID").toInt();
+
+    return client_transaction_id;
+}
+
+static JsonDocument _prepare_json_response (void)
+{
+    JsonDocument doc;
+
+    doc["ClientTransactionID"] = _get_client_transaction_id();
+    doc["ServerTransactionID"] = _get_server_transaction_id();
+    doc["ErrorNumber"] = 0;
+    doc["ErrorMessage"] = "";
+
+    return doc;
+}
+
+static void _set_response_error (JsonDocument& doc, const int error_number, const char * const description = nullptr)
+{
+    doc["ErrorNumber"] = error_number;
+
+    if (error_number)
+    {
+        if (description)
+            doc["ErrorMessage"] = description;
+        else
+            doc["ErrorMessage"] = "UnknownError";
+    }
+    else
+        doc["ErrorMessage"] = "";
+}
+
+static void _send_json_response (JsonDocument& doc)
+{
+    _server.sendHeader("Content-Type", "application/json");
+    String serialized;
+    serializeJson(doc, serialized);
+    _server.send(200, "application/json", serialized);
+}
+
+void ascom_alpaca::_handle_discovery(void)
+{
+    int packetSize = _udp.parsePacket();
+    if (packetSize > 0) 
+    {
+        char packetBuffer[256];
+        int len = _udp.readBytesUntil(0, packetBuffer, sizeof(packetBuffer) - 1);
+
+        if (len > 0) 
+        {
+            packetBuffer[len] = '\0';
+            String payload = String(packetBuffer);
+            if(payload.equalsIgnoreCase(F("alpacadiscovery1")))
+            {
+                const auto ip = _udp.remoteIP();
+                const auto port = _udp.remotePort();
+                _last_http_api_call = millis();
+
+                dprintf("\nDiscovery request received from %s:%u", ip.toString().c_str(), port);
+
+                // respond
+                JsonDocument doc;
+                doc["AlpacaPort"] = _port_device;
+
+                _udp.beginPacket(ip, port);
+                serializeJson(doc, _udp);
+                auto success = _udp.endPacket(); // BUG this doesn't seem to send anything despite returning success
+                if (!success)
+                {
+                    dprintf("\nSend failed with %d", _udp.getWriteError());
+                    _udp.clearWriteError();
+                }
+            }
+        }
+    }
+}
+
+const char *ascom_alpaca::get_uid(void)
+{
+  static char uid[21] = {};
+  if (!uid[0])
+    snprintf_P(uid, sizeof(uid), PSTR("RainSensor_%08X"), ESP.getChipId());
+
+  return uid;
+}
+
+uint32_t ascom_alpaca::get_last_api_call_time(void)
+{
+    return _last_http_api_call;
+}
+
+static void send_api_versions()
+{
+    _last_http_api_call = millis();
+    DEBUG_CALL_CYCLE
+
+    JsonDocument doc;
+
+    auto versions = doc["Value"].to<JsonArray>();
+    versions.add(1);
+
+    doc["ClientTransactionID"] = _get_client_transaction_id();
+    doc["ServerTransactionID"] = _get_server_transaction_id();
+
+    _send_json_response(doc);
+}
+
+static void send_api_description()
+{
+    _last_http_api_call = millis();
+    DEBUG_CALL_CYCLE
+
+    JsonDocument doc;
+
+    auto value = doc["Value"].to<JsonObject>();
+    value["ServerName"] = ascom_alpaca::get_uid();
+    value["Manufacturer"] = "Pliskin707";
+    value["ManufacturerVersion"] = "1.0.0.0";
+    value["Location"] = "nearby ;)";
+
+    doc["ClientTransactionID"] = _get_client_transaction_id();
+    doc["ServerTransactionID"] = _get_server_transaction_id();
+
+    _send_json_response(doc);
+}
+
+static void send_configured_devices()
+{
+    _last_http_api_call = millis();
+    DEBUG_CALL_CYCLE
+
+    JsonDocument doc;
+
+    auto value = doc["Value"].to<JsonArray>();
+
+    // Observing Conditions
+    auto item = value.add<JsonObject>();
+    item["DeviceName"] = "RainSensor";
+    item["DeviceType"] = "ObservingConditions";
+    item["DeviceNumber"] = 0;
+    auto unique_id = String(ascom_alpaca::get_uid());
+    unique_id += "_Conditions";
+    item["UniqueID"] = unique_id;
+
+    // Safety Monitor
+    item = value.add<JsonObject>();
+    item["DeviceName"] = "RainMonitor";
+    item["DeviceType"] = "SafetyMonitor";
+    item["DeviceNumber"] = 0;
+    unique_id = String(ascom_alpaca::get_uid());
+    unique_id += "_Monitor";
+    item["UniqueID"] = unique_id;
+
+    doc["ClientTransactionID"] = _get_client_transaction_id();
+    doc["ServerTransactionID"] = _get_server_transaction_id();
+
+    _send_json_response(doc);
+}
+
+static void send_connected_state()
+{
+    _last_http_api_call = millis();
+    DEBUG_CALL_CYCLE // gets called every 2000 ms by N.I.N.A.
+
+    auto doc = _prepare_json_response();
+
+    // TODO this has parameters; parse and use them!
+    // TODO this can be PUT or GET; handle them
+    doc["Value"] = true;
+
+    _send_json_response(doc);
+}
+
+static void send_rain_rate()
+{
+    _last_http_api_call = millis();
+    DEBUG_CALL_CYCLE
+
+    // TODO this has parameters; parse and use them!
+    auto doc = _prepare_json_response();
+    doc["Value"] = _rain_rate;
+
+    _send_json_response(doc);
+}
+
+static void send_device_description()
+{
+    _last_http_api_call = millis();
+    DEBUG_CALL_CYCLE
+
+    auto doc = _prepare_json_response();
+    doc["Value"] = "ESP8266 based device using a simple digital rain sensor";
+
+    _send_json_response(doc);
+}
+
+static void send_driver_info()
+{
+    _last_http_api_call = millis();
+    DEBUG_CALL_CYCLE
+
+    auto doc = _prepare_json_response();
+    doc["Value"] = "https://github.com/Pliskin707/ESP8266_RainSensor";
+
+    _send_json_response(doc);
+}
+
+static void send_driver_version()
+{
+    _last_http_api_call = millis();
+    DEBUG_CALL_CYCLE
+
+    auto doc = _prepare_json_response();
+    doc["Value"] = "1.0"; // see https://ascom-standards.org/newdocs/camera.html#Camera.DriverVersion
+
+    _send_json_response(doc);
+}
+
+static void send_interface_version()
+{
+    _last_http_api_call = millis();
+    DEBUG_CALL_CYCLE
+
+    auto doc = _prepare_json_response();
+    doc["Value"] = 4; // see https://ascom-standards.org/newdocs/camera.html#Camera.InterfaceVersion
+
+    _send_json_response(doc);
+}
+
+static void send_supported_actions()
+{
+    _last_http_api_call = millis();
+    DEBUG_CALL_CYCLE
+
+    auto doc = _prepare_json_response();
+    auto value = doc["Value"].to<JsonArray>(); // a list of all non-standard actions the device can perform
+    value.add("GetBatteryLevel"); // TODO implement (see https://ascom-standards.org/newdocs/camera.html#Camera.Action )
+
+    _send_json_response(doc);
+}
+
+static void send_average_period()
+{
+    _last_http_api_call = millis();
+    DEBUG_CALL_CYCLE // gets called every 2000 ms by N.I.N.A.
+
+    auto doc = _prepare_json_response();
+    doc["Value"] = _average_period_hours;
+
+    _send_json_response(doc);
+}
+
+static void change_average_period()
+{
+    _last_http_api_call = millis();
+    DEBUG_CALL_CYCLE
+
+    if (!_server.hasArg("AveragePeriod"))
+    {
+        _server.send(400, "Missing parameter \"AveragePeriod\"");
+        return;
+    }
+
+    auto doc = _prepare_json_response();
+
+    int average_period = _server.arg("AveragePeriod").toFloat();
+    if ((average_period < 0.0) || (average_period > 24.0))
+    {
+        _set_response_error(doc, 0x401, "Only values between 0..24 are allowed");
+    }
+    else
+    {
+        dprintf("Average period change to %.3f hours", average_period);
+        _average_period_hours = average_period;
+    }
+
+    _send_json_response(doc);
+}
+
+static void send_device_state_conditions()
+{
+    _last_http_api_call = millis();
+    DEBUG_CALL_CYCLE // gets called every 2000 ms by N.I.N.A.
+
+    auto doc = _prepare_json_response();
+    auto value = doc["Value"].to<JsonArray>();
+    auto item = value.add<JsonObject>();
+    item["Name"] = "RainRate";
+    // item["Value"] = _rain_rate;
+    item["Value"] = static_cast<float>(millis()) / 1000.0f; // TODO remove after debug
+
+    _send_json_response(doc);
+}
+
+static void send_device_state_monitor()
+{
+    _last_http_api_call = millis();
+    DEBUG_CALL_CYCLE  // gets called every 2000 ms by N.I.N.A.
+
+    auto doc = _prepare_json_response();
+    auto value = doc["Value"].to<JsonArray>();
+    auto item = value.add<JsonObject>();
+    item["Name"] = "IsSafe";
+    item["Value"] = _is_safe;
+
+    _send_json_response(doc);
+}
+
+static void send_not_implemented()
+{
+    _last_http_api_call = millis();
+    auto doc = _prepare_json_response();
+
+    char description[200];
+    const auto& uri = _server.uri();
+    const auto& property_name = uri.substring(uri.lastIndexOf('/') + 1);
+    snprintf_P(description, sizeof(description), PSTR("%s is not implemented"), property_name.c_str());
+    description[sizeof(description) - 1] = 0;
+    _set_response_error(doc, 0x400, description);
+
+    _send_json_response(doc);
+}
+
+void ascom_alpaca::begin(const uint16_t port_discovery, const uint16_t port_device)
+{
+    _port_discovery = port_discovery;
+    _port_device = port_device;
+
+    _server.on("/management/apiversions", send_api_versions);
+    _server.on("/management/v1/description", send_api_description);
+    _server.on("/management/v1/configureddevices", send_configured_devices);
+
+    _server.on("/setup/v1/observingconditions/0/setup", send_not_implemented);    // <- this is called if you press the gears symbol in N.I.N.A.
+    _server.on("/api/v1/observingconditions/0/connected", send_connected_state);
+    _server.on("/api/v1/observingconditions/0/interfaceversion", send_interface_version);
+    _server.on("/api/v1/observingconditions/0/description", send_device_description);
+    _server.on("/api/v1/observingconditions/0/driverinfo", send_driver_info);
+    _server.on("/api/v1/observingconditions/0/driverversion", send_driver_version);
+    _server.on("/api/v1/observingconditions/0/supportedactions", send_supported_actions);
+    _server.on("/api/v1/observingconditions/0/devicestate", send_device_state_conditions);
+
+    _server.on("/api/v1/observingconditions/0/rainrate", send_rain_rate);
+    _server.on("/api/v1/observingconditions/0/averageperiod", HTTP_GET, send_average_period);
+    _server.on("/api/v1/observingconditions/0/averageperiod", HTTP_PUT, change_average_period);
+    _server.on("/api/v1/observingconditions/0/cloudcover", send_not_implemented);
+    _server.on("/api/v1/observingconditions/0/dewpoint", send_not_implemented); // maybe later with a DHT11 or DHT22 (also requires `send_device_state_conditions()` to be updated)
+    _server.on("/api/v1/observingconditions/0/humidity", send_not_implemented); // maybe later with a DHT11 or DHT22 (also requires `send_device_state_conditions()` to be updated)
+    _server.on("/api/v1/observingconditions/0/pressure", send_not_implemented);
+    _server.on("/api/v1/observingconditions/0/skybrightness", send_not_implemented);
+    _server.on("/api/v1/observingconditions/0/skyquality", send_not_implemented);
+    _server.on("/api/v1/observingconditions/0/skytemperature", send_not_implemented);
+    _server.on("/api/v1/observingconditions/0/starfwhm", send_not_implemented);
+    _server.on("/api/v1/observingconditions/0/temperature", send_not_implemented); // maybe later with a DHT11 or DHT22 (also requires `send_device_state_conditions()` to be updated)
+    _server.on("/api/v1/observingconditions/0/winddirection", send_not_implemented);
+    _server.on("/api/v1/observingconditions/0/windgust", send_not_implemented);
+    _server.on("/api/v1/observingconditions/0/windspeed", send_not_implemented);
+
+    _server.on("/setup/v1/safetymonitor/0/setup", send_not_implemented);    // <- this is called if you press the gears symbol in N.I.N.A.
+    _server.on("/api/v1/safetymonitor/0/connected", send_connected_state);
+    _server.on("/api/v1/safetymonitor/0/interfaceversion", send_interface_version);
+    _server.on("/api/v1/safetymonitor/0/description", send_device_description);
+    _server.on("/api/v1/safetymonitor/0/driverinfo", send_driver_info);
+    _server.on("/api/v1/safetymonitor/0/driverversion", send_driver_version);
+    _server.on("/api/v1/safetymonitor/0/supportedactions", send_supported_actions);
+    _server.on("/api/v1/safetymonitor/0/devicestate", send_device_state_monitor);
+}
+
+void ascom_alpaca::loop(const bool connected)
+{
+    _is_safe = !rain_sensor.is_raining();
+    _rain_rate = (_is_safe ? 0.0f : 1.0f);
+
+    if (connected)
+    {
+        if (!_was_connected)
+        {
+            _udp.begin(_port_discovery);
+            _server.begin(_port_device);
+        }
+
+        _handle_discovery();
+        _server.handleClient();
+    }
+    else if (_was_connected)
+    {
+        _server.close();
+    }
+
+    _was_connected = connected;
+}
---
+++ ESP8266_AlpacaDevice/src/main.cpp
@@ -0,0 +1,102 @@
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiManager.h>
+#include "ota/ota.hpp"
+#include "projutils/projutils.hpp"
+#include "config.hpp"
+#include "AscomAlpaca/AscomAlpacaClass.hpp"
+
+using namespace pliskin;
+
+static bool mDNS_init_ok = false;
+static bool wifi_connected = false;
+
+WiFiManager wm;
+
+void setup() {
+  #ifndef DEBUG_PRINT
+  pinMode(LEDPIN, OUTPUT);
+  #else
+  Serial.begin(115200);
+  #endif
+
+  rain_sensor.begin();
+
+  // WiFi Manager - Async mode
+  WiFi.hostname(DEVICENAME);
+  wm.setConfigPortalBlocking(false);  // Non-blocking mode
+  wm.setConnectTimeout(20);           // Connection timeout in seconds
+  wm.setConnectRetries(3);            // Number of retries
+  
+  // Setup WiFi event callbacks
+  WiFi.onEvent([](WiFiEvent_t event) {
+    if (event == WIFI_EVENT_STAMODE_GOT_IP) {
+      wifi_connected = true;
+      dprintf("WiFi connected! IP: %s\n", WiFi.localIP().toString().c_str());
+
+      alpaca.begin(PORT_ALPACA_DISCOVERY, PORT_ALPACA_DEVICE);
+
+      // Initialize mDNS only after WiFi is connected
+      if (!mDNS_init_ok) {
+        mDNS_init_ok = MDNS.begin(DEVICENAME);
+        if (mDNS_init_ok) {
+          dprintf("mDNS initialized\n");
+        }
+      }
+      
+      // Initialize OTA only after WiFi is connected
+      ota::begin(DEVICENAME);
+    } 
+    else if (event == WIFI_EVENT_STAMODE_DISCONNECTED) {
+      wifi_connected = false;
+      dprintf("WiFi disconnected\n");
+    }
+  });
+  
+  wifi_set_sleep_type(LIGHT_SLEEP_T);
+
+  dprintf("\n\nUID: %s\n\n", alpaca.get_uid());
+  
+  // Start WiFiManager (will start config portal if not connected)
+  wm.autoConnect(DEVICENAME);
+}
+
+void loop() {
+  const uint32_t time = millis();
+  static uint32_t next = 0;
+
+  // WiFiManager async processing
+  wm.process();
+
+  // Wifi status
+  const bool connected = WiFi.isConnected();
+  #ifndef DEBUG_PRINT
+  digitalWrite(LEDPIN, !connected);
+  #endif
+
+  // mDNS
+  if (mDNS_init_ok)
+    MDNS.update();
+
+  // OTA
+  if (connected)
+    ota::handle();
+
+  alpaca.loop(connected);
+
+  if (time >= next)
+  {
+    next = time + 10000;
+    dprintf("\nSystime: %lu ms; WLAN: %sconnected (as %s)", time, (connected ? "":"dis"), connected ? WiFi.localIP().toString().c_str() : "N/A");
+  }
+
+  if ((millis() - alpaca.get_last_api_call_time()) > 1000uL)
+  {
+    dprintf("\nSleep");
+    delay(500); // sleep some time to conserver battery
+  }
+  else
+    yield(); // more commands may follow in quick succession
+}
